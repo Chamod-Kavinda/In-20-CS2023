6c0b8c2d1c9263a62fac6cf7d77fef8377dbc037
Chamod-Kavinda <chamodkavinda20@gmail.com>
2023-04-04 20:29:01 +0530

    Week 6 In Class Assignment added

--- Week 6/Question3.cpp
+++ Week 6/Question3.cpp
@@ -1,188 +1,220 @@
 #include <iostream>
 #include <chrono>
-
 using namespace std;
 using namespace std::chrono;
 
-#define MAX 100
+const int MAX_SIZE = 100; // maximum size of the stack
 
-class StackUsingArray {
-	int stack[MAX];
-	int top = -1;
-
-public:
-	void push(int x) {
-		top++;
-
-		if (top == MAX) {
-			top--;
-			cerr << "ERROR: Stack Overflow\n";
-		}
-		else {
-			stack[top] = x;
-		}
-	}
-
-	int pop() {
-		if (top == -1) {
-			cerr << "ERROR: Stack Underflow\n";
-		}
-		else {
-			int x = stack[top];
-			top--;
-			return x;
-		}
-	}
-
-	bool isEmpty() {
-		return (top == -1);
-	}
-
-	bool isFull() {
-		return (top == MAX - 1);
-	}
-
-	int stackTop() {
-		if (isEmpty())
-			cerr << "ERROR: Stack Underflow\n";
-
-		return stack[top];
-	}
-
-	void display() {
-		if (top == -1)
-			cerr << "Stack Underflow\n";
-
-		cout << "[ ";
-		for (int i = top; i >= 0; i--)
-			cout << stack[i] << ", " << "\n";
-		cout << " ]\n";
-	}
+struct StackArray {
+    int top; // index of the top element in the stack
+    int arr[MAX_SIZE]; // array to store the elements in the stack
 };
+
+// function to push an element onto the stack
+void push(StackArray& S, int x) {
+    S.top++;
+    if (S.top >= MAX_SIZE) {
+        cout << "Stack Overflow\n";
+        S.top--;
+    }
+    else {
+        S.arr[S.top] = x;
+    }
+}
+
+// function to pop an element from the stack
+int pop(StackArray& S) {
+    if (S.top < 0) {
+        cout << "Stack Underflow\n";
+        return -1;
+    }
+    else {
+        int val = S.arr[S.top];
+        S.top--;
+        return val;
+    }
+}
+
+// function to check if the stack is empty
+bool is_empty(StackArray& S) {
+    return (S.top < 0);
+}
+
+bool is_full(StackArray& S) {
+    return (S.top >= MAX_SIZE - 1);
+}
+
+int stackTop(StackArray& S) {
+    if (S.top >= MAX_SIZE - 1) {
+        cout << "Stack is empty\n";
+        return -1;
+    }
+    else {
+        return S.arr[S.top];
+    }
+}
+
+void display(StackArray& S) {
+    if (S.top >= MAX_SIZE - 1) {
+        cout << "[]\n";
+        return;
+    }
+    else {
+        cout << "[";
+        for (int j = 0; j <= S.top; j++) {
+            cout << S.arr[j] << ", ";
+        }
+        cout << "]\n";
+    }
+}
 
 struct Node {
-	int data = 0;
-	Node* next = nullptr;
+    int data;
+    Node* next;
 };
 
-class StackUsingList {
-	Node* top = nullptr;
-
-public:
-	void push(int x) {
-		Node* newNode = new Node;
-		newNode->data = x;
-		newNode->next = top;
-		top = newNode;
-	}
-
-	int pop() {
-		if (isEmpty())
-			cerr << "ERROR: Stack Underflow\n";
-
-		int x = top->data;
-		Node* temp = top;
-		top = top->next;
-		delete temp;
-
-		return x;
-	}
-
-	bool isEmpty() {
-		return top == nullptr;
-	}
-
-	int stackTop() {
-		if (isEmpty())
-			cerr << "ERROR: Stack Underflow\n";
-
-		return top->data;
-	}
-
-	void display() {
-		if (isEmpty())
-			cerr << "ERROR: Stack Underflow\n";
-
-		cout << "[ ";
-		Node* node = top;
-		while (node != nullptr) {
-			cout << node->data << ", ";
-			node = node->next;
-		}
-		cout << " ]\n";
-	}
+struct StackList {
+    Node* top;
 };
+
+// function to create a new node
+Node* create_node(int x) {
+    Node* node = new Node;
+    node->data = x;
+    node->next = NULL;
+    return node;
+}
+
+// function to push an element onto the stack
+void push(StackList& S, int x) {
+    Node* node = create_node(x);
+    if (S.top == NULL) {
+        S.top = node;
+    }
+    else {
+        node->next = S.top;
+        S.top = node;
+    }
+}
+
+// function to pop an element from the stack
+int pop(StackList& S) {
+    if (S.top == NULL) {
+        cout << "Stack Underflow\n";
+        return -1;
+    }
+    else {
+        int x = S.top->data;
+        Node* temp = S.top;
+        S.top = S.top->next;
+        delete temp;
+
+        return x;
+    }
+}
+
+// function to check if the stack is empty
+bool is_empty(StackList& S) {
+    return (S.top == NULL);
+}
+
+int stack_top(StackList& S) {
+    if (S.top == NULL) {
+        cout << "Stack Underflow\n";
+        return -1;
+    }
+    else {
+        return S.top->data;
+    }
+}
+
+void display(StackList S) {
+    if (S.top == NULL) {
+        cout << "[]\n";
+    }
+    else {
+        Node* temp = S.top;
+        cout << "[";
+        while (temp != NULL) {
+            cout << temp->data << ", ";
+            temp = temp->next;
+        }
+        cout << "]\n";
+    }
+}
 
 #define TIMES 1000
 
 int main() {
-	auto start1 = high_resolution_clock::now();
-	for (int i = 0; i < TIMES; i++) {
-		StackUsingArray stackArray;
 
-		stackArray.push(8);
-		stackArray.push(10);
-		stackArray.push(5);
-		stackArray.push(11);
-		stackArray.push(15);
-		stackArray.push(23);
-		stackArray.push(6);
-		stackArray.push(18);
-		stackArray.push(20);
-		stackArray.push(17);
+    auto start1 = high_resolution_clock::now();
+    for (int i = 0; i < TIMES; i++) {
+        StackArray S;
+        S.top = -1;
+        push(S, 8);
+        push(S, 10);
+        push(S, 5);
+        push(S, 11);
+        push(S, 15);
+        push(S, 23);
+        push(S, 6);
+        push(S, 18);
+        push(S, 20);
+        push(S, 17);
+        display(S);
 
-		stackArray.display();
+        pop(S);
+        pop(S);
+        pop(S);
+        pop(S);
+        pop(S);
+        display(S);
 
-		for (int j = 0; j < 5; j++)
-			stackArray.pop();
+        push(S, 4);
+        push(S, 30);
+        push(S, 3);
+        push(S, 1);
+        display(S);
+    }
+    auto stop1 = high_resolution_clock::now();
+    auto duration1 = duration_cast<microseconds>(stop1 - start1);
+    auto time1 = duration1.count() / TIMES;
 
-		stackArray.display();
+    auto start2 = high_resolution_clock::now();
+    for (int i = 0; i < TIMES; i++) {
+        StackList S;
+        S.top = NULL;
+        push(S, 8);
+        push(S, 10);
+        push(S, 5);
+        push(S, 11);
+        push(S, 15);
+        push(S, 23);
+        push(S, 6);
+        push(S, 18);
+        push(S, 20);
+        push(S, 17);
+        display(S);
 
-		stackArray.push(4);
-		stackArray.push(30);
-		stackArray.push(3);
-		stackArray.push(1);
+        pop(S);
+        pop(S);
+        pop(S);
+        pop(S);
+        pop(S);
+        display(S);
 
-		stackArray.display();
-	}
-	auto stop1 = high_resolution_clock::now();
-	auto duration1 = duration_cast<microseconds>(stop1 - start1);
-	auto time1 = duration1.count() / TIMES;
+        push(S, 4);
+        push(S, 30);
+        push(S, 3);
+        push(S, 1);
+        display(S);
+    }
+    auto stop2 = high_resolution_clock::now();
+    auto duration2 = duration_cast<microseconds>(stop2 - start2);
+    auto time2 = duration2.count() / TIMES;
 
-	auto start2 = high_resolution_clock::now();
-	for (int i = 0; i < TIMES; i++)
-	{
-		StackUsingList stackList;
+    cout << "Time taken by StackUsingArray: " << time1 << " microseconds" << endl;
+    cout << "Time taken by StackUsingList: " << time2 << " microseconds" << endl;
 
-		stackList.push(8);
-		stackList.push(10);
-		stackList.push(5);
-		stackList.push(11);
-		stackList.push(15);
-		stackList.push(23);
-		stackList.push(6);
-		stackList.push(18);
-		stackList.push(20);
-		stackList.push(17);
-
-		stackList.display();
-
-		for (int j = 0; j < 5; j++)
-			stackList.pop();
-
-		stackList.display();
-
-		stackList.push(4);
-		stackList.push(30);
-		stackList.push(3);
-		stackList.push(1);
-
-		stackList.display();
-	}
-	auto stop2 = high_resolution_clock::now();
-	auto duration2 = duration_cast<microseconds>(stop2 - start2);
-	auto time2 = duration2.count() / TIMES;
-
-	cout << "Time taken by StackUsingArray: " << time1 << " microseconds" << endl;
-	cout << "Time taken by StackUsingList: " << time2 << " microseconds" << endl;
+    return 0;
 }
