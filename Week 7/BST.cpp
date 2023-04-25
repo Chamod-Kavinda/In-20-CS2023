b29b49156b4090c2bf0c66c0df3743abc7ff1e49
Chamod-Kavinda <chamodkavinda20@gmail.com>
2023-04-25 16:31:02 +0530

    Week 7 In Class Assignment added

---
+++ Week 7/BST.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+using namespace std;
+
+struct node {
+    int key;
+    struct node* left, * right;
+};
+
+// Create a new node
+struct node* newNode(int key) {
+    struct node* temp1 = new node;
+    temp1->key = key;
+    temp1->left = temp1->right = NULL;
+    return temp1;
+}
+
+// Inorder traversal
+void traverseInOrder(struct node* root) {
+    if (root == NULL) {
+        return;
+    }
+    traverseInOrder(root->left);
+    cout << root->key << " ";
+    traverseInOrder(root->right);
+}
+
+// Insert a node
+struct node* insertNode(struct node* root, int key) {
+    if (root == NULL) {
+        return newNode(key);
+    }
+    if (key < root->key) {
+        root->left = insertNode(root->left, key);
+    }
+    else if (key > root->key) {
+        root->right = insertNode(root->right, key);
+    }
+    return root;
+}
+
+
+struct node* minValueNode(struct node* node) {
+    struct node* temp2 = node;
+    while (temp2 && temp2->left != NULL) {
+        temp2 = temp2->left;
+    }
+    return temp2;
+}
+
+// Deleting a node
+struct node* deleteNode(struct node* root, int key) {
+    if (root == NULL) {
+        return root;
+    }
+    if (key < root->key) {
+        root->left = deleteNode(root->left, key);
+    }
+    else if (key > root->key) {
+        root->right = deleteNode(root->right, key);
+    }
+    else {
+        if (root->left == NULL) {
+            struct node* temp = root->right;
+            delete root;
+            return temp;
+        }
+        else if (root->right == NULL) {
+            struct node* temp = root->left;
+            delete root;
+            return temp;
+        }
+        struct node* temp = minValueNode(root->right);
+        root->key = temp->key;
+        root->right = deleteNode(root->right, temp->key);
+    }
+    return root;
+}
+
+// Driver code
+int main() {
+    struct node* root = NULL;
+
+    int operation;
+    int operand;
+    cin >> operation;
+
+    while (operation != -1) {
+        switch (operation) {
+        case 1: // insert
+            cin >> operand;
+            root = insertNode(root, operand);
+            cin >> operation;
+            break;
+        case 2: // delete
+            cin >> operand;
+            root = deleteNode(root, operand);
+            cin >> operation;
+            break;
+        default:
+            cout << "Invalid Operator!\n";
+            return 0;
+        }
+    }
+
+    traverseInOrder(root);
+}
